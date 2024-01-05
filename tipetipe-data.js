b4b46f68c52503150200809c1993f93f06727f8b
Prasetya Tegar Fitriaji <58251507+prasetyatf@users.noreply.github.com>
2024-01-06 04:45:49 +0700

    Update tipetipe-data.js

--- tipetipe-data.js
+++ tipetipe-data.js
@@ -3,15 +3,18 @@
 
 // object
 const iniObject = new Object({ nama: "tegar", usia: 22 });
+
 // accessing and modifying object
 console.log(iniObject["nama"]);
 iniObject["usia"] = 99;
 console.log(iniObject);
+
 // destructuring object
 const { nama, usia } = iniObject;
 console.log(nama, usia);
 const { pekerjaan = false } = iniObject;
 console.log(nama, pekerjaan);
+
 // spreading object
 const ituObject = new Object({ skill: "javascript" });
 const newObject = new Object({ ...iniObject, ...ituObject });
@@ -19,13 +22,16 @@ console.log(newObject);
 
 // array
 const iniArray = new Array(["aku", "adalah", "tegar"]);
+
 // accessing and modifying array
 console.log(iniArray[0]);
 iniArray.splice(2, 1, "prasetya");
 console.log(iniArray);
+
 // destructuring array
 const [, , namasaya] = iniArray;
 console.log(`destructuring array: ${namasaya}`);
+
 // spreading array
 const ituArray = ["dan", "prasetya"];
 console.log([...iniArray, ...ituArray]);
@@ -37,6 +43,7 @@ const iniMap = new Map([
   ["1b", "tegar"],
 ]);
 console.log(iniMap);
+
 // accesnya pakai .get(), menambahnya pakai .set(), menghapus .delete()
 console.log(iniMap["1a"]);
 console.log(iniMap.get("1a")); //akan error jika Map declarenya ga seharusnya
@@ -48,6 +55,7 @@ console.log(iniMap);
 // Set (unindexed, unordered, unique)
 const iniSet = new Set([1, 1, 1, 2, 3]);
 console.log(iniSet);
+
 // .add(), .delete()
 iniSet.add(9);
 console.log(iniSet);
