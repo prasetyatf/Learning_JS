e643b1bce1b8f07f30ceb098b87fafc73ac9f4f3
Prasetya Tegar Fitriaji <58251507+prasetyatf@users.noreply.github.com>
2024-07-08 14:37:23 +0700

    Update arrayMethod.js

--- arrayMethod.js
+++ arrayMethod.js
@@ -24,3 +24,68 @@ console.log(hasilForeach);
 
 const hasilForeach2 = arrayKTP.forEach((item) => console.log(item)); // callback berupa menampilkan nilai
 console.log(hasilForeach2);
+
+// input: array
+const barisAngka = [1,2,3,4]
+
+// FOREACH : mengaplikasikan fungsi ke setiap nilai
+// output : undefined
+
+const forits = barisAngka.forEach((value, index, arr) => {
+    return value+1
+})
+console.log(`forEach: ${forits}`)
+
+
+// MAP : mengaplikasikan fungsi ke setiap nilai
+// output : array
+
+const mep = barisAngka.map((value, index, arr) => {
+    return value+1
+})
+console.log(`map: ${mep}`)
+
+
+// FILTER : mengecek semua nilai dengan kondisi tertentu
+// output: array
+
+const saring = barisAngka.filter((val, index, arr)=>{
+    return val<4
+})
+console.log(`filter: ${saring}`)
+
+
+// FIND : mengecek nilai-nilai sampai memenuhi kodisi tertentu
+// output : value
+
+const cari = barisAngka.find((val, index)=>{
+    return val > 2
+})
+console.log(`find: ${cari}`)
+
+
+// REDUCE : mengaplikasikan fungsi ke semua elemen untuk memproduksi satu value
+// output : value
+
+const REDUCE = barisAngka.reduce((totalPrevValue, value, index, arr)=>{
+    return totalPrevValue + value
+})
+console.log(`reduce: ${REDUCE}`)
+
+
+// EVERY (analogi logika AND) : mengecek semua nilai sesuai kondisi tertentu
+// output: boolean
+
+const semua = barisAngka.every((val, index, arr)=>{
+    return val > 1
+})
+console.log(`every: ${semua}`)
+
+
+// SOME(analogi logika OR) : mengecek semua nilai sesuai kondisi tertentu
+// output: boolean
+
+const beberapa = barisAngka.some((val, index, arr)=>{
+    return val > 1
+})
+console.log(`some: ${beberapa}`)
